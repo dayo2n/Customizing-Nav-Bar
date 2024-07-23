55e5eaaf8ec09048458da019ffdb4c5e04d08e56
Dayeon Moon <zena247@interparktriple.com>
2024-07-23 18:46:26 +0900

    type #colorLiteral() to show color picker

--- Customizing-Nav-Bar/Sources/CustomAppearanceViewController.swift
+++ Customizing-Nav-Bar/Sources/CustomAppearanceViewController.swift
@@ -80,7 +80,7 @@ final class CustomAppearanceViewController: UITableViewController {
     }
     
     private func applyColorBackgroundAppearance() {
-        let tintColor = UIColor.systemGreen
+        let tintColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         
         let standardAppearance = UINavigationBarAppearance()
         standardAppearance.configureWithOpaqueBackground()
